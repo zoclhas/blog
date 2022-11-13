69022dc14fc9acf644bf6bb5d569c382a38f91ca
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-11-13 12:51:26 +0400

    Dunno whys it still not responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,39 +1,47 @@
 import { createTheme, NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import Head from "next/head";
 
 import ThemeSwitch from "../components/ThemeSwitch";
 
 import "../styles/main.css";
 
 const lightTheme = createTheme({
-	type: "light",
-	theme: {},
+    type: "light",
+    theme: {},
 });
 
 const darkTheme = createTheme({
-	type: "dark",
-	theme: {},
+    type: "dark",
+    theme: {},
 });
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<NextThemesProvider
-			defaultTheme="light"
-			attribute="class"
-			value={{
-				light: lightTheme.className,
-				dark: darkTheme.className,
-			}}
-		>
-			<NextUIProvider>
-				<Layout>
-					<Component {...pageProps} />
-				</Layout>
-				<ThemeSwitch />
-			</NextUIProvider>
-		</NextThemesProvider>
-	);
+    return (
+        <NextThemesProvider
+            defaultTheme="light"
+            attribute="class"
+            value={{
+                light: lightTheme.className,
+                dark: darkTheme.className,
+            }}
+        >
+            <NextUIProvider>
+                <Head>
+                    <title>Zoclhas | Blog</title>
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1.0, width=device-width"
+                    />
+                </Head>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+                <ThemeSwitch />
+            </NextUIProvider>
+        </NextThemesProvider>
+    );
 }
 
 export default MyApp;
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,14 +1,14 @@
-import fs from 'fs';
-import matter from 'gray-matter';
-import md from 'markdown-it';
+import fs from "fs";
+import matter from "gray-matter";
+import md from "markdown-it";
 
-import { Head } from 'next/head'
+import Head from "next/head";
 
 export async function getStaticPaths() {
-    const files = fs.readdirSync('posts');
+    const files = fs.readdirSync("posts");
     const paths = files.map((fileName) => ({
         params: {
-            slug: fileName.replace('.md', ''),
+            slug: fileName.replace(".md", ""),
         },
     }));
     return {
@@ -18,7 +18,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-    const fileName = fs.readFileSync(`posts/${slug}.md`, 'utf-8');
+    const fileName = fs.readFileSync(`posts/${slug}.md`, "utf-8");
     const { data: frontmatter, content } = matter(fileName);
     return {
         props: {
@@ -31,10 +31,19 @@ export async function getStaticProps({ params: { slug } }) {
 export default function PostPage({ frontmatter, content }) {
     return (
         <div>
-            {/* <Head><title>{frontmatter.title}</title></Head> */}
-            <article className='prose'>
+            <Head>
+                <title>{frontmatter.title}</title>
+                <link
+                    rel="stylesheet"
+                    href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/default.min.css"
+                />
+                <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/highlight.min.js"></script>
+            </Head>
+            <article className="prose">
                 <h1>{frontmatter.title}</h1>
-                <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+                <div
+                    dangerouslySetInnerHTML={{ __html: md().render(content) }}
+                />
             </article>
         </div>
     );
