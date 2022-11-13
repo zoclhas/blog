bf656bca4109b63adb6b5880cc2669079f2972fb
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-11-13 22:50:10 +0400

    Idk how its responsive- Im not going to touch

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -28,7 +28,7 @@ export async function getStaticProps({ params: { slug } }) {
 
 export default function PostPage({ frontmatter, content }) {
     return (
-        <div className="prose mx-auto">
+        <div className="prose prose-img:rounded-xl lg:prose-xs">
             <h1>{frontmatter.title}</h1>
             <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
         </div>
