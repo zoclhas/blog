a96156fa66efa9ff5f758caf9219383dfffb8602
Zoclhas <70476453+Zoclhas@users.noreply.github.com>
2022-11-14 15:53:58 +0400

    Oops- wrong page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { createTheme, NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import Layout from "../components/Layout";
-import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/main.css";
@@ -27,22 +26,6 @@ function MyApp({ Component, pageProps }) {
             }}
         >
             <NextUIProvider>
-                <Head>
-                    <title>Zoclhas | Blog</title>
-                    <meta property="og:title" content="Zoclhas | Blog" />
-                    <meta property="og:url" content="https://blog.zochy.xyz" />
-                    <meta
-                        property="og:description"
-                        content="Personal blogging site by Zoc. Random updates, thoughts on stuff and more!"
-                    />
-                    <meta
-                        property="og:image"
-                        content={`https://blog.zochy.xyz/images/blog-1.png`}
-                    />
-                    <meta name="description" content="Personal blogging site by Zoc. Random updates, thoughts on stuff and more!" />
-                    <meta name="theme-color" content="#ffeacb" />
-                    <meta name="twitter:card" content="summary_large_image" />
-                </Head>
                 <Layout>
                     <Component {...pageProps} />
                     <Analytics />
