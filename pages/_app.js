bf220499af20d92e61393028b72b90553808e26b
Zoclhas <zoclhas@gmail.com>
2023-01-07 18:56:42 +0400

    Uhh stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import { Analytics } from "@vercel/analytics/react";
+
+import "../styles/main.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <main className="container">
+                <Component {...pageProps} />
+            </main>
+            <Analytics />
+        </>
+    );
 }
